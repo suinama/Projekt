f1a7472dd0536dd831b06327fd17e2f06a4e2ff2
suinama <opklmjhurfgujhhyfdcx@gmail.com>
2019-11-10 16:06:43 +0100

    projekt

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 
 using namespace std;
 // szukanie wartoœci min max w kolumnie
-    for (int column = 0; column < 5; column++) {
+    for (int column = 0; column < 6; column++) {
         int min = matrix[0][column];
         int max = matrix[0][column];
-        for (int row = 0; row < 5; row++) {
+        for (int row = 0; row < 6; row++) {
             // je¿eli aktualna komórka jest mniejsza ni¿ min to zaktualizuj min
             if (matrix[row][column] < min) {
                 min = matrix[row][column];
