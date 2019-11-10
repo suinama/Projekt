02f0eb133b8c4c658337d359424a6b34244f7b0b
suinama <opklmjhurfgujhhyfdcx@gmail.com>
2019-11-10 15:53:35 +0100

    projekt

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include "time.h"
 
 using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+// szukanie wartoœci min max w kolumnie
+    for (int column = 0; column < 5; column++) {
+        int min = matrix[0][column];
+        int max = matrix[0][column];
+        for (int row = 0; row < 5; row++) {
+            // je¿eli aktualna komórka jest mniejsza ni¿ min to zaktualizuj min
+            if (matrix[row][column] < min) {
+                min = matrix[row][column];
+            }
+            if (matrix[row][column] > max) {
+                max = matrix[row][column];
+            }
+            cout << "min in column " << column << " equals " << min << endl;
+            cout << "max in column " << column << " equals " << max << endl;
+        }
+    }
 }
+    int main() {
+        // wywo³anie zawartoœci metody
+        ex18();
+        return 0;
+    }
+
+
