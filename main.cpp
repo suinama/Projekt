11f27bd523e7b709259cfdd7073203a3eeed5c48
suinama <57593037+suinama@users.noreply.github.com>
2019-11-10 16:13:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "time.h"
 
 using namespace std;
-// szukanie wartoœci min max w kolumnie
-    for (int column = 0; column < 5; column++) {
+// szukanie wartoÅ“ci min max w kolumnie
+    for (int column = 0; column < 6; column++) {
         int min = matrix[0][column];
         int max = matrix[0][column];
-        for (int row = 0; row < 5; row++) {
-            // je¿eli aktualna komórka jest mniejsza ni¿ min to zaktualizuj min
+        for (int row = 0; row < 6; row++) {
+            // jeÂ¿eli aktualna komÃ³rka jest mniejsza niÂ¿ min to zaktualizuj min
             if (matrix[row][column] < min) {
                 min = matrix[row][column];
             }
@@ -23,7 +23,7 @@ using namespace std;
     }
 }
     int main() {
-        // wywo³anie zawartoœci metody
+        // wywoÂ³anie zawartoÅ“ci metody
         ex18();
         return 0;
     }
